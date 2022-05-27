a595ddfe80211963f55b6534e9c432ec66498042
veysel basbaydar <vgbasbaydar@gmail.com>
2022-05-26 18:54:54 -0700

    finished LinkedList

--- linked2.js
+++ linked2.js
@@ -25,6 +25,7 @@ class LinkedList {
 
   insertFirst(data) {
     this.head = new Node(data, this.head);
+    this.size++;
   }
 
   //insert lastNode
@@ -36,17 +37,97 @@ class LinkedList {
       this.head = node;
     } else {
       let current = this.head; //start beginin
-      while (current == null) {}
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+      this.size++;
     }
   }
 
   //insert at index
+  insertAt(data, index) {
+    //if index is out of range;
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    // if first index
+    if (index == 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
 
+    const node = new Node(data);
+
+    let current, previous;
+
+    // Set current to first
+    current = this.head;
+
+    let count = 0;
+    while (count < index) {
+      previous = current; //node before index
+      count++;
+      current = current.next; //node after the index
+    }
+    node.next = current;
+    previous.next = node;
+    this.size++;
+  }
   //Get at index
+  getAt(index) {
+    // let current = this.head;
+    // let count = 0;
+    // while (current) {
+    //   if (count === index) {
+    //     console.log(current.data);
+    //   }
+    //   count++;
+    //   current = current.next;
+    // }
+    // return null;
+    if (index < 0 || index > this.size) {
+      return;
+    }
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    console.log(current.data);
+  }
 
   // Remove at index
 
+  removeAt(index) {
+    if (index < 0 || index > this.size) {
+      return null;
+    }
+    let current = this.head;
+    let previous;
+    let count = 0;
+    //remove first
+    if (index == 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+    }
+
+    this.size--;
+  }
+
   //clear list
+
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
 
   // Print list data
 
@@ -63,6 +144,13 @@ const ll = new LinkedList();
 ll.insertFirst(100);
 ll.insertFirst(200);
 ll.insertFirst(300);
+ll.insertLast(400);
+//ll.insertAt(500, 2);
+//ll.insertAt(500, 0);
+//ll.getAt(2);
+///ll.removeAt(3);
+
+ll.clearList();
 ll.printList();
 //console.log(ll);
 
