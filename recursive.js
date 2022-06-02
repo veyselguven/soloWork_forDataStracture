e07804c50d7cb10480bc78d11b288ee87118910c
veysel basbaydar <vgbasbaydar@gmail.com>
2022-06-01 17:38:11 -0700

    non recursive fibonacci

--- recursive.js
+++ recursive.js
@@ -14,4 +14,22 @@ function topla(number) {
   }
 }
 
-topla(5);
+//topla(5);
+
+function factorial(number) {
+  if (number === 0) return 1;
+  else {
+    return number * factorial(number - 1);
+  }
+}
+console.log(factorial(5));
+
+var fac = (num) => {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+console.log(fac(5));
