d51345874cfaeb515966912ccb904f0e6cf3a9fe
veysel basbaydar <vgbasbaydar@gmail.com>
2022-06-04 12:07:38 -0700

    some practise on sync

---
+++ quiz.js
@@ -0,0 +1,8 @@
+//let modal = document.querySelector("#result");
+console.log("model");
+setTimeout(function () {
+  // modal.classList.remove("hidden");
+  console.log("modal in time out");
+}, 3000);
+
+console.log("Result shown");
