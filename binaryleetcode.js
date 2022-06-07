f1237579354aaf35b9d876cf7ebb24cd1c68cae3
veysel basbaydar <vgbasbaydar@gmail.com>
2022-06-06 23:04:14 -0700

    binary search

---
+++ binaryleetcode.js
@@ -0,0 +1,12 @@
+const preOrderTraversal = function (root) {
+  const stack = [root];
+  const result = [];
+  while (stack.length) {
+    let node = stack.pop();
+
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+  return result;
+};
