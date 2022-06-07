4e46d661bb07ad8308dbfcff5fe1e8e318ca153d
veysel basbaydar <vgbasbaydar@gmail.com>
2022-06-07 11:39:21 -0700

    inOrder traversal

--- binaryleetcode.js
+++ binaryleetcode.js
@@ -20,10 +20,22 @@ var recursivePreOrder = function () {
     if (!node) return null;
     result.push(node.val);
     if (node.left) preOrder(node.left);
-    //result.push(node.val); on order
+    //result.push(node.val); in order
     if (node.right) preOrder(node.right);
     //result.push(node.val); post order
   };
   preOrder(root);
+  return result;
+};
+var inorderTraversal = function (root) {
+  if (!root) return [];
+  const result = [];
+  const inOrder = (node) => {
+    if (!node) return null;
+    if (node.left) inOrder(node.left);
+    result.push(node.val);
+    if (node.right) inOrder(node.right);
+  };
+  inOrder(root);
   return result;
 };
