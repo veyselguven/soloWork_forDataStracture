f475597795dfda2a721d93d3e66f7ae8002d2308
veysel basbaydar <vgbasbaydar@gmail.com>
2022-06-06 23:34:21 -0700

    preorder stack

--- binaryleetcode.js
+++ binaryleetcode.js
@@ -1,4 +1,5 @@
 const preOrderTraversal = function (root) {
+  if (!root) return [];
   const stack = [root];
   const result = [];
   while (stack.length) {
@@ -8,5 +9,21 @@ const preOrderTraversal = function (root) {
     if (node.right) stack.push(node.right);
     if (node.left) stack.push(node.left);
   }
+  return result;
+};
+
+var recursivePreOrder = function () {
+  if (!root) return null;
+  const result = [];
+
+  const preOrder = (node) => {
+    if (!node) return null;
+    result.push(node.val);
+    if (node.left) preOrder(node.left);
+    //result.push(node.val); on order
+    if (node.right) preOrder(node.right);
+    //result.push(node.val); post order
+  };
+  preOrder(root);
   return result;
 };
