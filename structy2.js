556889b3c24adf6e231fd3b178f3fd52aa6adf3b
veysel basbaydar <vgbasbaydar@gmail.com>
2022-06-28 01:52:47 -0700

    linkedin practise

--- structy2.js
+++ structy2.js
@@ -1,1 +1,65 @@
 //console.log("asddv");
+const getNodeValue = (head, index) => {
+  // todo
+  let current = head;
+  let counter = 0;
+
+  while (counter !== index) {
+    current = current.next;
+    if (current === null) return null;
+    counter++;
+  }
+  //console.log(current.val);
+  return current.val;
+};
+
+
+/*
+//Time O(n); n nodes
+//Space 0(1);
+const linkedListFind = (head, target) => {
+  // todo
+  let current=head;
+  while(current!==null){
+    if(current.val===target){
+      return true;
+    } 
+    current=current.next; 
+  } 
+return false
+};
+*/
+//O(n) time complextiy
+//O(n) space complextiy
+const linkedListFind=(head,target)=>{
+    if(head===null) return false;
+    if(head.val===target)  return true;
+    return linkedListFind(head.next,targe   t)
+  }
+
+
+  //0(n);Time
+//O(1):Space
+
+const sumList = (head) => {
+    // todo
+    let sum=0;
+    let current=head;
+    
+    while(current!==null){
+      sum+=current.val;
+      current=current.next;
+    }
+    return sum
+  };
+  
+  
+  /*
+  
+  //O(n) time
+  //O(1 ) space
+  const sumList=(head)=>{
+   if(head===null) return 0;
+    return head.val+sumList(head.next)
+  }
+  */
