2551471b44bd6b31f2dc6b34d7145805700e3039
veysel basbaydar <vgbasbaydar@gmail.com>
2022-08-17 08:03:33 +0300

    anagram

--- backtracking.js
+++ backtracking.js
@@ -1,1 +1,22 @@
 console.log("test");
+
+var groupAnagrams = function (strs) {
+  debugger;
+  for (let i = 0; i < strs.length; i++) {
+    let str = strs[i].split("").sort().join();
+
+    if (!obj.hasOwnProperty(str)) {
+      obj[str] = count;
+      arr[count] = [strs[i]];
+      count++;
+    } else {
+      arr[obj[str]].push(strs[i]);
+    }
+  }
+
+  return arr;
+};
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+//console.log(groupAnagrams(strs));
