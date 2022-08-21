c1e2ea402ebe374a45259f686f89b695cb4a9854
veysel basbaydar <vgbasbaydar@gmail.com>
2022-08-21 08:18:04 +0300

    other solution

--- hastable.js
+++ hastable.js
@@ -21,9 +21,36 @@ var isIsomorphic = function (s, t) {
 let s = "egg",
   t = "add";
 
-console.log(isIsomorphic(s, t));
+//console.log(isIsomorphic(s, t));
 
 let a = "veysel";
 for (let i in a) {
   console.log(i);
+}
+
+function pigIt(str) {
+  str = str.split(" ");
+  let result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (/([a-zA-Z])/.test(str[i])) {
+      let word = str[i].slice(1) + str[i][0] + "ay";
+      result.push(word);
+    } else {
+      result.push(s[i]);
+    }
+  }
+  return result.join(" ");
+}
+
+console.log(pigIt("Pig latin is cool"));
+
+function pigIt2(str) {
+  //Code here
+  return str
+    .split(" ")
+    .map((word) => {
+      return word.substring(1) + word[0] + "ay";
+    })
+    .join(" ");
 }
