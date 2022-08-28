b89af755aee95e16b1eecf2f69fbd4dd5c94f58d
veysel basbaydar <vgbasbaydar@gmail.com>
2022-08-28 17:21:11 +0300

    level binary sumaverage

--- sbinary4.js
+++ sbinary4.js
@@ -1,6 +1,36 @@
-for (let i = 0; i < 5; i++) {
-  for (let j = 3; j >= 1; j--) {
-    console.log("*");
+var averageOfLevels = function (root) {
+  if (!root) return [];
+
+  const result = [];
+  const stack = [{ node: root, level: 0 }];
+
+  while (stack.length > 0) {
+    let currentNode = stack.pop();
+    // console.log("currentNode==>",currentNode)
+    if (result.length === currentNode.level) {
+      result.push([currentNode.node.val]);
+    } else {
+      result[currentNode.level].push(currentNode.node.val);
+    }
+    if (currentNode.node.right)
+      stack.push({
+        node: currentNode.node.right,
+        level: currentNode.level + 1,
+      });
+    if (currentNode.node.left)
+      stack.push({ node: currentNode.node.left, level: currentNode.level + 1 });
   }
-  console.log("/n");
-}
+  console.log(result);
+
+  let final = [];
+
+  for (let i = 0; i < result.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < result[i].length; j++) {
+      sum += result[i][j];
+    }
+    let av = sum / result[i].length;
+    final.push(av);
+  }
+  return final;
+};
