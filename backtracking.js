e9a1cfbb81a5aeda6b92bc0a1c48cce2392e528d
veyselguven <vgbasbaydar@gmail.com>
2023-05-11 23:26:21 -0700

    duplicate zero

--- backtracking.js
+++ backtracking.js
@@ -1,24 +1,62 @@
-console.log("test");
+// console.log("test");
 
-var groupAnagrams = function (strs) {
-  //debugger;
-  let arr = [];
-  let obj = {};
-  let count = 0;
-  for (let i = 0; i < strs.length; i++) {
-    let str = strs[i].split("").sort().join();
-    if (!obj.hasOwnProperty(str)) {
-      obj[str] = count;
-      arr[count] = [strs[i]];
-      count++;
+// var groupAnagrams = function (strs) {
+//   //debugger;
+//   let arr = [];
+//   let obj = {};
+//   let count = 0;
+//   for (let i = 0; i < strs.length; i++) {
+//     let str = strs[i].split("").sort().join();
+//     if (!obj.hasOwnProperty(str)) {
+//       obj[str] = count;
+//       arr[count] = [strs[i]];
+//       count++;
+//     } else {
+//       arr[obj[str]].push(strs[i]);
+//     }
+//   }
+
+//   return arr;
+// };
+
+// strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+// console.log(groupAnagrams(strs));
+// var duplicateZeros = function (arr) {
+//   // debugger;
+//   let setArry = new Array(arr.length);
+//   let i = 0;
+//   //    i
+//   // [1,0,2,3,0,4,5,0]
+//   while (i < setArry.length) {
+//     if (arr[i] === 0) {
+//       setArry[i] = 0;
+//       setArry[i + 1] = 0;
+//       i++;
+//     } else if (setArry[i] === 0) {
+//       setArry[i + 1] = arr[i];
+//       i++;
+//     } else {
+//       setArry[i] = arr[i];
+//       i++;
+//     }
+//   }
+//   return setArry;
+// };
+var duplicateZeros = function (arr) {
+  if (arr == null || arr.length == 0) throw new Error("illegal input");
+  //move in place, so no extra space
+  debugger;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 0) {
+      //that means must remove last one in arr
+      for (let j = arr.length - 1; j > i; j--) {
+        arr[j] = arr[j - 1];
+      }
+      i++;
     } else {
-      arr[obj[str]].push(strs[i]);
+      continue;
     }
   }
-
-  return arr;
 };
-
-strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
-
-console.log(groupAnagrams(strs));
+console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]));
--- string.js
+++ string.js
@@ -1,43 +1,60 @@
-var strStr = function (haystack, needle) {
-  //  debugger;
-  if (haystack === needle || needle === "") {
-    return 0;
-  }
-  for (let i = 0; i < haystack.length; i++) {
-    if (haystack[i] === needle[0]) {
-      let temp = haystack.substring(i, i + needle.length);
-      if (temp === needle) {
-        return i;
-      }
+// var strStr = function (haystack, needle) {
+//   //  debugger;
+//   if (haystack === needle || needle === "") {
+//     return 0;
+//   }
+//   for (let i = 0; i < haystack.length; i++) {
+//     if (haystack[i] === needle[0]) {
+//       let temp = haystack.substring(i, i + needle.length);
+//       if (temp === needle) {
+//         return i;
+//       }
+//     }
+//   }
+//   return -1;
+// };
+
+// let haystack = "sadbutsad",
+//   needle = "sad";
+
+// console.log(strStr(haystack, needle));
+
+// var countBinarySubstrings = function (s) {
+//   //  debugger;
+//   let count = 0;
+//   let i = 1;
+//   let prev = 0;
+//   let curr = 1;
+
+//   while (i < s.length) {
+//     // "00110011"
+//     if (s.charAt(i - 1) !== s.charAt(i)) {
+//       count += Math.min(prev, curr);
+//       prev = curr;
+//       curr = 1;
+//     } else {
+//       curr++;
+//     }
+//     i++;
+//   }
+//   return (count += Math.min(prev, curr));
+// };
+
+// console.log(countBinarySubstrings("11000110"));
+
+var numberOfSteps = function (num) {
+  debugger;
+  let result = 0;
+  while (num > 0) {
+    if (num % 2 === 0) {
+      num = num / 2;
+      result++;
+    } else if (num % 2 !== 0) {
+      num = num - 1;
+      result++;
     }
   }
-  return -1;
+  return result;
 };
 
-let haystack = "sadbutsad",
-  needle = "sad";
-
-console.log(strStr(haystack, needle));
-
-var countBinarySubstrings = function (s) {
-  //  debugger;
-  let count = 0;
-  let i = 1;
-  let prev = 0;
-  let curr = 1;
-
-  while (i < s.length) {
-    // "00110011"
-    if (s.charAt(i - 1) !== s.charAt(i)) {
-      count += Math.min(prev, curr);
-      prev = curr;
-      curr = 1;
-    } else {
-      curr++;
-    }
-    i++;
-  }
-  return (count += Math.min(prev, curr));
-};
-
-console.log(countBinarySubstrings("11000110"));
+console.log(numberOfSteps(12));
