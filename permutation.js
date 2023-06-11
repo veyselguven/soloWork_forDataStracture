c91c32825f9ec1295ba75a6d81ee6bc80888087a
veyselguven <vgbasbaydar@gmail.com>
2023-06-11 09:27:48 -0700

    permute

--- permutation.js
+++ permutation.js
@@ -8,32 +8,57 @@
 
 // reverse from peak ro end of  arr
 
-console.log("afsdfbb");
+// console.log("afsdfbb");
 
-function pairwise(arr, arg) {
-  // debugger;
-  let total = 0;
-  for (let i = 0; i < arr.length; i++) {
-    let currentSum = 0;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === arg) {
-        currentSum += i + j;
-      }
+// function pairwise(arr, arg) {
+//   // debugger;
+//   let total = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     let currentSum = 0;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === arg) {
+//         currentSum += i + j;
+//       }
+//     }
+//     total += currentSum;
+//   }
+//   return total;
+// }
+
+// pairwise([1, 4, 2, 3, 0, 5], 7);
+// let v = [1, 2, 4, 5, 6, 7, 3];
+
+// console.log(v.slice(0));
+
+// const body = 50;
+// if (50 > 49) {
+//   const water = 1.4;
+//   console.log(`${body} naberrr ${water}`);
+// }
+// //console.log(body);
+// console.log(`${body}fdg`);
+var permute = function (nums) {
+  debugger;
+  // global result
+  const result = [];
+
+  // dfs recursive helper
+  const dfs = (i, nums) => {
+    // base case
+    if (i === nums.length) {
+      result.push(nums.slice());
+      return;
     }
-    total += currentSum;
-  }
-  return total;
-}
+    // [1,2,3];
+    // dfs recursive case
+    for (let j = i; j < nums.length; j++) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      dfs(i + 1, nums);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  };
+  dfs(0, nums);
+  return result;
+};
 
-pairwise([1, 4, 2, 3, 0, 5], 7);
-let v = [1, 2, 4, 5, 6, 7, 3];
-
-console.log(v.slice(0));
-
-const body = 50;
-if (50 > 49) {
-  const water = 1.4;
-  console.log(`${body} naberrr ${water}`);
-}
-//console.log(body);
-console.log(`${body}fdg`);
+console.log(permute([1, 2, 3]));
