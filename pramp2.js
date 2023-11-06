deb8108e9c65803f734b038b6b65e3357bc8a797
veyselguven <vgbasbaydar@gmail.com>
2023-11-06 14:39:49 -0800

    added some math algothm

--- pramp2.js
+++ pramp2.js
@@ -121,3 +121,14 @@ var plusOne = function (digits) {
   digits.unshift(1);
   return digits;
 };
+var addDigits = function (num) {
+  let sum = num;
+  while (sum >= 10) {
+    strArray = sum.toString().split("");
+    sum = 0;
+    for (let i = 0; i < strArray.length; i++) {
+      sum += Number(strArray[i]);
+    }
+  }
+  return sum;
+};
