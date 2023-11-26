fb01ba4657b318df792d1da63611a041708904e1
veyselguven <vgbasbaydar@gmail.com>
2023-11-26 11:46:38 -0800

    added some logic for flat array

--- backtracking.js
+++ backtracking.js
@@ -60,3 +60,24 @@ var duplicateZeros = function (arr) {
   }
 };
 console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]));
+
+let arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6, [7, 8], 9],
+  [10, 11, 12],
+];
+const customFlat = (arr, depth = 1) => {
+  let result = [];
+  arr.forEach((ar) => {
+    if (Array.isArray(ar) && depth > 0) {
+      result.push(...customFlat(ar, depth - 1));
+    } else {
+      result.push(ar);
+    }
+    return result;
+  });
+};
+
+// [].concat(...arr)
+// arr.flat(2)
