0e8514f2227679db21a7cf2abb303bb3077057fa
veyselguven <vgbasbaydar@gmail.com>
2024-01-10 22:20:13 -0800

    added some algoruthm

--- mostgoogle.js
+++ mostgoogle.js
@@ -7,3 +7,20 @@ const maxPathSum = (root) => {
 module.exports = {
   maxPathSum,
 };
+
+var reverseWords = function (s) {
+  s = s.split(" ");
+  // console.log(s) // [ "Let's", 'take', 'LeetCode', 'contest' ]
+  let result = [];
+  for (let i = 0; i < s.length; i++) {
+    let value = s[i].split("").reverse().join("");
+    console.log(value);
+    result.push(value);
+  }
+  return result.join(" ");
+};
+
+var isBoomerang = function (points) {
+  const [[x1, y1], [x2, y2], [x3, y3]] = points;
+  return (y1 - y2) * (x1 - x3) != (y1 - y3) * (x1 - x2);
+};
