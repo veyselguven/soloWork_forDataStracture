f6c5ea2605d78eb55f61dffd60cca47b4f938e8b
veyselguven <vgbasbaydar@gmail.com>
2024-01-16 23:12:50 -0800

    rotate array

--- mostgoogle.js
+++ mostgoogle.js
@@ -24,3 +24,37 @@ var isBoomerang = function (points) {
   const [[x1, y1], [x2, y2], [x3, y3]] = points;
   return (y1 - y2) * (x1 - x3) != (y1 - y3) * (x1 - x2);
 };
+
+var rotate1 = function (nums, k) {
+  let a = nums.slice(-k);
+  // console.log(a)//[ 5, 6, 7 ]
+  let b = nums.slice(0, k);
+  //  console.log(b)//[ 1, 2, 3, 4 ]
+  //[5,6,7,1,2,3,4]
+  let c = a.concat(b);
+  // console.log(c);
+  return c;
+};
+let nums = [1, 2, 3, 4, 5, 6, 7],
+  k = 3;
+// let nums = [-1, -100, 3, 99],
+//   k = 2;
+console.log(rotate1(nums, k));
+
+const revNums = (nums, start, end) => {
+  while (start < end) {
+    [nums[start], nums[end]] = [nums[end], nums[start]];
+    start++;
+    end--;
+  }
+};
+
+var rotate = function (nums, k) {
+  k = k % nums.length;
+  console.log(k);
+  nums.reverse();
+  revNums(nums, 0, k - 1);
+  revNums(nums, k, nums.length - 1);
+};
+
+console.log(2 % 4);
