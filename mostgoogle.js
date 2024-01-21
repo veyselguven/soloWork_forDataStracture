863d16477af06f26636c0f31a179d3339fd7c432
veyselguven <vgbasbaydar@gmail.com>
2024-01-21 14:41:46 -0800

    added countSegment Algrothm

--- mostgoogle.js
+++ mostgoogle.js
@@ -117,3 +117,18 @@ var merge = function (intervals) {
   }
   return result;
 };
+
+
+var countSegments = function(s) {
+     // Split the input string by spaces
+    const segments = s.split(' ');
+    
+    let count = 0;
+    // Iterate through the segments and count non-empty ones
+    for (const segment of segments) {
+        if (segment !== '') {
+        count++;
+        }
+    }   
+    return count;
+};
