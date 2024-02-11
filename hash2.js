3ffb1244efda814dfb282baf1e653b4a36b39173
veyselguven <vgbasbaydar@gmail.com>
2024-02-11 08:49:25 -0800

    polidrome || algortihn

--- hash2.js
+++ hash2.js
@@ -98,3 +98,26 @@ var uncommonFromSentences = function (s1, s2) {
   }
   return result;
 };
+
+var uncommonFromSentences2 = function (s1, s2) {
+  let a = [...s1.split(" "), ...s2.split(" ")];
+  const obj = {};
+
+  for (let i of a) {
+    if (i in obj) {
+      obj[i] += 1;
+    } else {
+      obj[i] = 1;
+    }
+  }
+
+  a = [];
+
+  for (let i in obj) {
+    if (obj[i] == 1) {
+      a.push(i);
+    }
+  }
+
+  return a;
+};
