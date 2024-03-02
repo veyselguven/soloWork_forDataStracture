1cd03775fb26db97a2a98e5d781066b70ce82c88
veyselguven <vgbasbaydar@gmail.com>
2024-03-02 08:27:12 -0800

    added an grid algortihm

---
+++ oracle.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function (grid) {
+  let perimeter = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        perimeter += 4;
+        if (j > 0 && grid[i][j - 1] === 1) {
+          perimeter -= 2;
+        }
+        if (i > 0 && grid[i - 1][j] === 1) {
+          perimeter -= 2;
+        }
+      }
+    }
+  }
+  return perimeter;
+};
+
+/* 
+1.Create perimeter variable
+2.Loop through grid
+    a.Loop thorugh inner array.
+        i.Condition if value is "1".
+            1.Add perimeter by 4.
+            2.if the block before is "1".
+                a.Subsctract perimeter by "2".
+            3.if the block above is "1"
+                a.Subsctract perimeter by 2
+3.Return perimeter   
+
+ */
