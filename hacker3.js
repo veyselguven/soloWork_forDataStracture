55355ed1a9ce9f65ed53bec2e819dcdcd018916f
veyselguven <vgbasbaydar@gmail.com>
2024-04-02 19:25:03 -0700

    added an algorthim

--- hacker3.js
+++ hacker3.js
@@ -12,3 +12,30 @@ function findMedian(arr) {
 
 console.log(findMedian([1, 3, 3, 6, 7, 8, 9]));
 console.log(findMedian([1, 2, 3, 4, 5, 6, 8, 9]));
+
+console.log({} == {}); // false
+console.log({} === {}); // false
+//This condition will always return 'false'
+//since JavaScript compares objects by reference, not value.
+
+var largestAltitude = function (gain) {
+  let currentGain = 0;
+  let result = 0;
+
+  for (let num of gain) {
+    currentGain += num;
+    result = Math.max(currentGain, result);
+  }
+
+  return result;
+};
+
+// var largestAltitude = function(gain) {
+//     let result=[0];
+//     let sum=0;
+//     for(let i =0; i<gain.length; i++){
+//             sum+=gain[i]
+//             result.push(sum)
+//     }
+//    return Math.max(...result)
+// };
