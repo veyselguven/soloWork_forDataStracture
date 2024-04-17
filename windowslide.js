5e85dfbc29fed82c663444f6096cd1614fe59776
veyselguven <vgbasbaydar@gmail.com>
2024-04-17 00:14:59 -0700

    windows slide

--- windowslide.js
+++ windowslide.js
@@ -235,3 +235,18 @@ var lengthOfLongestSubstring = function (s) {
   }
   return max;
 };
+
+var maxPower = function (s) {
+  let power = 1;
+
+  let start = 0;
+  for (let end = 1; end < s.length; end++) {
+    if (s[start] !== s[end]) {
+      start = end;
+    }
+
+    power = Math.max(power, end - start + 1);
+  }
+
+  return power;
+};
