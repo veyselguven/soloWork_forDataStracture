b348ed3abba97afa9923e05517e25b4bb2aab030
veyselguven <vgbasbaydar@gmail.com>
2024-05-28 13:36:24 -0700

    added linked algorthm

--- linked2.js
+++ linked2.js
@@ -242,3 +242,29 @@ var removeZeroSumSublists = function (head) {
   }
   return dummy.next;
 };
+
+var addTwoNumbers = function (l1, l2) {
+  let carry = 0;
+  let previousNode = new ListNode();
+  const headNode = previousNode;
+  while (l1 || l2 || carry) {
+    let val1 = 0;
+    let val2 = 0;
+    if (l1) {
+      val1 = l1.val;
+      l1 = l1.next;
+    }
+    if (l2) {
+      val2 = l2.val;
+      l2 = l2.next;
+    }
+    let sum = val1 + val2 + carry;
+    carry = Math.floor(sum / 10); //sum > 9 ? 1 : 0
+    let digit = sum % 10;
+    console.log(digit);
+    const currentNode = new ListNode(digit);
+    previousNode.next = currentNode;
+    previousNode = currentNode;
+  }
+  return headNode.next;
+};
--- subsetEEquality.js
+++ subsetEEquality.js
@@ -63,10 +63,19 @@ var addToArrayForm = function (num, k) {
 let num = [1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3],
   k = 516;
 //console.log(addToArrayForm(num, k));
-console.log(Number("12630717197566440063"));
+//console.log(Number("12630717197566440063"));
 
 // let string = "assadfbg";
 
 // for (let s of string) {
 //   console.log(s);
 // }
+
+// console.log("5" + 3);
+// console.log("5" - 3);
+// console.log(typeof NaN);
+// console.log(typeof null);
+
+// console.log(NaN === NaN);
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
