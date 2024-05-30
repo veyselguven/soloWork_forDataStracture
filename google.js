c94cdd01c9a5cbaa423e4970920d2bd25d597f48
veyselguven <vgbasbaydar@gmail.com>
2024-05-29 17:59:36 -0700

    added an algorthm

--- google.js
+++ google.js
@@ -135,3 +135,14 @@ var isPalindrome = function (x) {
 
   return true;
 };
+
+var removeElement = function (nums, val) {
+  var zeroStartIdx = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[zeroStartIdx] = nums[i];
+      zeroStartIdx++;
+    }
+  }
+  return zeroStartIdx;
+};
