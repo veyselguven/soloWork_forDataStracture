774514e99b1c8fa911810f8bf94b39657c6c8f4d
veyselguven <vgbasbaydar@gmail.com>
2024-07-08 14:34:38 -0700

    added windows slide method

--- subsetEEquality.js
+++ subsetEEquality.js
@@ -113,3 +113,16 @@ Remove palindromic subsequence "baab" then "b".
 var removePalindromeSub = function (s) {
   return s === s.split("").reverse().join("") ? 1 : 2;
 };
+var findMaxAverage = function (nums, k) {
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+
+  let max = sum;
+  for (let i = k; i < nums.length; i++) {
+    sum = sum - nums[i - k] + nums[i];
+    max = Math.max(max, sum);
+  }
+  return max / k;
+};
